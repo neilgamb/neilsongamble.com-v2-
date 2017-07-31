8c47640cd3abeefdd267a9ff3d907e7db6a89f13
Neilson Gamble <neilgamb@Neilsons-MacBook-Pro.local>
2017-07-31 13:12:50 -0400

    dev horizontal scroll complete

--- js/app.js
+++ js/app.js
@@ -1,9 +1,10 @@
-$(window).on("scroll", function() {
-    if($(window).scrollTop() > 50) {
+$(window).on("scroll", function () {
+    if ($(window).scrollTop() > 50) {
         $("header").addClass("active");
     } else {
         //remove the background property so it comes transparent again (defined in your css)
-       $("header").removeClass("active");
+        $("header").removeClass("active");
     }
 });
+
 
